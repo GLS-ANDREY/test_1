32a11ac8e5aee5e27eebf72928814f13c2c8640a
андрей <zhukov_ai@outlook.com>
2021-03-15 11:36:37 +0300

    тест

--- __main__.py
+++ __main__.py
@@ -1,5 +1,9 @@
 import wrap_py, wrap_py.ru
 from wrap_py import world, sprite
 
-world.create_world(600, 600)
-sprite.add_sprite("flappy_bird", 200, 200)
+world.create_world(850, 850)
+world.set_world_background_color([255, 0, 3])
+sprite.add_sprite("rocket_man", 425, 425, True, "rocket")
+@wrap_py.on_mouse_down(1)
+def taskat():
+    print(5)
